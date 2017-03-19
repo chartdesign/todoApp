cee1a80b1ee562570e909d1953db9cd820e464c2
hartdesign <mayjahleaguebeats@gmail.com>
2017-03-19 05:26:33 +0000

    removed code bloat

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,5 @@
 class TodosController < ApplicationController 
+  before_action :set_todo, only: [:edit, :update, :show, :destroy]
   
   def new
     @todo = Todo.new
@@ -16,15 +17,15 @@ class TodosController < ApplicationController
   end
   
   def show
-    @todo = Todo.find(params[:id])
+    
   end
   
   def edit
-    @todo = Todo.find(params[:id])
+    
   end
   
   def update
-    @todo = Todo.find(params[:id])
+    
     if @todo.update(todo_params)
       flash[:notice] = "Todo was succesfully updated."
       redirect_to todo_path(@todo)
@@ -38,7 +39,7 @@ class TodosController < ApplicationController
   end
   
   def destroy
-    @todo = Todo.find(params[:id])  
+      
     @todo.destroy
     flash[:notice] = "Todo was deleted succesfully."
     redirect_to todos_path
@@ -47,6 +48,10 @@ class TodosController < ApplicationController
   
   
   private
+  
+    def set_todo
+      @todo = Todo.find(params[:id])
+    end
     def todo_params
       params.require(:todo).permit(:name, :description)
     end
